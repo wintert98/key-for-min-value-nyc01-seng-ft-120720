95b65bf1db1881d4fd86386baa06fe1483abf46c
Thomas Winter <wintert98@gmail.com>
2020-11-28 15:50:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_value = 1000
-  lowest_key = ""
+  lowest_key = 
   name_hash.each do |key, value|
     if value < lowest_value
       lowest_value = value
